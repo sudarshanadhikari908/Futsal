9085afd345da5be9fd15bfb47cf150df674b55e2
Sudarshan <amstersudhan633@gmail.com>
2020-09-08 11:56:31 +0545

    Change 5

--- SneaX/urls.py
+++ SneaX/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+admin.site.site_header = "Football is Life"
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home.urls')),
--- home/views.py
+++ home/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render,HttpResponse
 from django.contrib import messages
+from .models import Contact
 
 # Create your views here.
 def home(request):
@@ -9,7 +10,11 @@ def home(request):
 
 def contact(request):
     messages.success(request, 'Welcome To Contact')
-    
-        
-
+    if request.method == 'POST':
+       name = request.POST['name']
+       email = request.POST['email']
+       phone = request.POST['phone']
+       content = request.POST['content']
+       contact = Contact(name = name, email=email, phone=phone, content=content)
+       contact.save()
     return render(request, 'home/contact.html')
